9367af8b5e37a7337c33a05c103775f7ae72f2a3
Rakhi Jain <rakhi.jain@ongraph.com>
2024-07-11 12:35:11 +0530

    certification section adding

---
+++ database/migrations/2024_07_11_122643_create_user_certifications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_certifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('certification_id');
+            $table->foreign('certification_id')->nullable()->references('id')->on('certifications')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->nullable()->references('id')->on('users')->onDelete('cascade');
+            $table->date('issue_date'); // Issue date of the certification
+            $table->date('expiry_date')->nullable(); // Expiry date (optional)
+            $table->string('certificationId')->nullable(); // Certification ID (optional)
+            $table->string('certification_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_certifications');
+    }
+};
