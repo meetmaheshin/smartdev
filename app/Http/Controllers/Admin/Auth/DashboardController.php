fda0637c2804b31ed043f8a2abd47d01b8f6c51b
Rakhi Jain <rakhi.jain@ongraph.com>
2024-07-11 11:59:33 +0530

    add profile section in admin

--- app/Http/Controllers/Admin/Auth/DashboardController.php
+++ app/Http/Controllers/Admin/Auth/DashboardController.php
@@ -10,16 +10,32 @@ use App\Models\User;
 class DashboardController extends Controller
 {
 
-  public function __construct() {
-    $this->middleware(['auth']);
-}
+    public function __construct() {
+        $this->middleware(['auth']);
+    }
 
-  public function index(Request $request){
-    $data['project']=Project::where('job','new')->count();
-    $data['client']=User::where('status',1)->where('is_admin','1')->count();
-    $data['freelancer']=User::where('status',1)->where('is_admin','0')->count();
+    public function index(Request $request){
+      $data['project']=Project::where('job','new')->count();
+      $data['client']=User::where('status',1)->where('is_admin','1')->count();
+      $data['freelancer']=User::where('status',1)->where('is_admin','0')->count();
+      return view('admin.dashboard',$data);
+    }
 
-    return view('admin.dashboard',$data);
-    
-  }
+    // Profile 
+
+    public function profile(Request $request){
+      return view('admin.profile');
+    }
+
+    public function saveProfile(Request $request){
+      $user = User::whereId(auth()->user()->id)->update(array(
+        'firstname'=>$request->firstname,
+        'lastname'=>$request->lastname,
+        'email'=>$request->email,
+        'phone_no'=>$request->phone_no,
+
+      ));
+      return redirect()->route('admin.profile')->with('success','Profile Updated');
+    }
+
 }
