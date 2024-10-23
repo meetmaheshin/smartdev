afb7a58b2e116352171ec3367e4dd2f6578dd6a9
kushal varin <kushalvarin@gmial.com>
2024-10-23 17:41:23 +0530

    Added Correct cta's of cryptocurrency-development page

--- resources/views/static/scroll-card-section.blade.php
+++ resources/views/static/scroll-card-section.blade.php
@@ -55,12 +55,12 @@
      </div>
     </div>
     <div align="center" class="contentall" style="padding-top: 0;">
-     <a class="nectar-button medium regular accent-color regular-button nec-btn" data-color-override="false" data-hover-color-override="false" data-hover-text-color-override="#fff" href="https://calendly.com/blockchainappfactory/15min?hide_gdpr_banner=1" rel="nofollow" style="border-radius: 30px !important;" target="_blank">
+     {{-- <a class="nectar-button medium regular accent-color regular-button nec-btn" data-color-override="false" data-hover-color-override="false" data-hover-text-color-override="#fff" href="https://calendly.com/blockchainappfactory/15min?hide_gdpr_banner=1" rel="nofollow" style="border-radius: 30px !important;" target="_blank">
       <span>
        Schedule A Meeting
       </span>
-     </a>
-     <a href="#newsletter-form-sec" rel="nofollow" style="border-radius: 30px !important; text-transform:none !important;">
+     </a> --}}
+     <a href="{{ url('consultation') }}" rel="nofollow" style="border-radius: 30px !important; text-transform:none !important;">
       Connect With Our Experts
      </a>
     </div>
